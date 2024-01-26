d6d7c675b3e04602be9db0fd5be01bd29f01af8b
Raffaele Vitale <raffa.vitale26@gmail.com>
2024-01-26 11:35:56 +0100

    Aggiunta delle animazioni sulla cartella

--- generale.js
+++ generale.js
@@ -1,8 +1,7 @@
-const bottone = document.getElementById('animazione-bottone');
+function animazione() {
+  // Seleziona l'elemento che deve animarsi
+  const elemento = document.getElementById("animazione-bottone");
 
-bottone.addEventListener('mouseup', () => {
-  bottone.classList.remove('animate__bounceOutDown');
-  setTimeout(() => {
-    bottone.classList.add('animate__bounceInDown');
-  }, 0); // Aggiungi la classe di entrata dopo un breve ritardo
-});
+  // Aggiungi la classe di animazione
+  elemento.classList.add("animate__animated", "animate__backOutDown");
+}
